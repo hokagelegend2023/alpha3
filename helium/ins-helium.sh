525763864b4eee4b7959abd3587f2c9a079f12dc
hokagelegend2023 <136045532+hokagelegend2023@users.noreply.github.com>
2023-08-16 18:09:10 +0700

    Update ins-helium.sh

--- helium/ins-helium.sh
+++ helium/ins-helium.sh
@@ -2,8 +2,7 @@
 # =========================================
 # Quick Setup | Script Setup Manager
 # Edition : Stable Edition 1.0
-# Auther  : givpn
-# Kuota Habisss
+# Auther  : HoKage Legend
 # (C) Copyright 2023
 # =========================================
 # pewarna hidup
@@ -16,11 +15,11 @@ wget -q -O /usr/bin/m-system https://raw.githubusercontent.com/givpn/AutoScriptX
 chmod +x /usr/bin/m-system
 rm -rf /usr/bin/wbmn
 echo -e "\e[1;32m Start download Panel Webmin.. \e[0m"
-wget -q -O /usr/bin/wbmn https://raw.githubusercontent.com/givpn/AutoScriptXray/master/helium/webmin/wbmn.sh
+wget -q -O /usr/bin/wbmn https://raw.githubusercontent.com/hokagelegend2023/alpha3/main/helium/webmin/wbmn.sh
 chmod +x /usr/bin/wbmn
 rm -rf /usr/bin/helium
 echo -e "\e[1;32m Start download Panel ADS Block.. \e[0m"
-wget -q -O /usr/bin/helium https://raw.githubusercontent.com/givpn/AutoScriptXray/master/helium/helium.sh
+wget -q -O /usr/bin/helium https://raw.githubusercontent.com/hokagelegend2023/alpha3/main/helium/helium.sh
 chmod +x /usr/bin/helium
 echo -e "\e[1;32m Setup done Please wait.. \e[0m"
 sleep 2
