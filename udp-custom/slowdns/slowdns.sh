2c93ed6035cd494fc9f52c2dc2858a11ea48632e
hokagelegend2023 <136045532+hokagelegend2023@users.noreply.github.com>
2023-08-18 22:11:34 +0700

    Update slowdns.sh

--- udp-custom/slowdns/slowdns.sh
+++ udp-custom/slowdns/slowdns.sh
@@ -1,8 +1,7 @@
 # =========================================
 # Quick Setup | Script Setup Manager
-# Edition : Stable Edition 1.0
-# Auther  : givpn
-# Kuota Habisss
+# Edition : Stable Edition Alpha 3.0
+# Auther  : Hokage Legend
 # (C) Copyright 2023
 # =========================================
 # pewarna hidup
@@ -60,10 +59,10 @@ service sshd restart
 #konfigurasi slowdns
 rm -rf /etc/slowdns
 mkdir -m 777 /etc/slowdns
-wget -q -O /etc/slowdns/server.key "https://raw.githubusercontent.com/fisabiliyusri/SLDNS/main/slowdns/server.key"
-wget -q -O /etc/slowdns/server.pub "https://raw.githubusercontent.com/fisabiliyusri/SLDNS/main/slowdns/server.pub"
-wget -q -O /etc/slowdns/sldns-server "https://raw.githubusercontent.com/fisabiliyusri/SLDNS/main/slowdns/sldns-server"
-wget -q -O /etc/slowdns/sldns-client "https://raw.githubusercontent.com/fisabiliyusri/SLDNS/main/slowdns/sldns-client"
+wget -q -O /etc/slowdns/server.key "https://raw.githubusercontent.com/hokagelegend2023/alpha1/main/slowdn/server.key"
+wget -q -O /etc/slowdns/server.pub "https://raw.githubusercontent.com/hokagelegend2023/alpha1/main/slowdn/server.pub"
+wget -q -O /etc/slowdns/sldns-server "https://raw.githubusercontent.com/hokagelegend2023/alpha1/main/slowdn/sldns-server"
+wget -q -O /etc/slowdns/sldns-client "https://raw.githubusercontent.com/hokagelegend2023/alpha1/main/slowdn/sldns-client"
 cd
 chmod +x /etc/slowdns/server.key
 chmod +x /etc/slowdns/server.pub
@@ -71,14 +70,14 @@ chmod +x /etc/slowdns/sldns-server
 chmod +x /etc/slowdns/sldns-client
 
 cd
-#wget -q -O /etc/systemd/system/client-sldns.service "https://raw.githubusercontent.com/fisabiliyusri/SLDNS/main/slowdns/client-sldns.service"
-#wget -q -O /etc/systemd/system/server-sldns.service "https://raw.githubusercontent.com/fisabiliyusri/SLDNS/main/slowdns/server-sldns.service"
+#wget -q -O /etc/systemd/system/client-sldns.service "https://raw.githubusercontent.com/hokagelegend2023/alpha1/main/slowdn/client-sldns.service"
+#wget -q -O /etc/systemd/system/server-sldns.service "https://raw.githubusercontent.com/hokagelegend2023/alpha1/main/slowdn/server-sldns.service"
 
 cd
 #install client-sldns.service
 cat > /etc/systemd/system/client-sldns.service << END
 [Unit]
-Description=Client SlowDNS By SL
+Description=Client SlowDNS By Hokage Legend
 Documentation=https://nekopoi.care
 After=network.target nss-lookup.target
 
@@ -99,7 +98,7 @@ cd
 #install server-sldns.service
 cat > /etc/systemd/system/server-sldns.service << END
 [Unit]
-Description=Server SlowDNS By SL
+Description=Server SlowDNS By Hokage Legend
 Documentation=https://nekopoi.care
 After=network.target nss-lookup.target
 
