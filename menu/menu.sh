1424c89000d69948d0644237a7ba0f7ecdd4dc7c
hokage <142867374+hokage2023@users.noreply.github.com>
2023-08-24 14:18:28 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -90,7 +90,6 @@ if [[ $openvpn == "running" ]]; then
 else
     status_openvpn="${RED}OFF${NC}"
 fi
-/etc/init.d/openvpn status
 
 # // SSH Websocket Proxy Dropbear
 ssh_wstdb=$( systemctl status ws-stunnel.service | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
@@ -226,7 +225,7 @@ echo -e "$COLOR1│$NC [ SSH WS   : ${status_wst} ]    [ XRAY : ${status_xray} ]
 echo -e "$COLOR1│$NC                                                          $COLOR1│$NC"
 echo -e "$COLOR1│$NC [ SW-SHOCK : ${status_xray} ]    [ DROPBEAR : ${status_dropbear} ]  [ Slow DNS : ${status_sldns} ]$COLOR1│$NC"
 echo -e "$COLOR1│$NC                                                          $COLOR1│$NC"
-echo -e "$COLOR1│$NC [ UDP-COSTUM : ${status_udp} ]  [ GRPC : ${status_xray} ]      [ RUNNING : ${status_openvpn} ] $COLOR1│$NC"
+echo -e "$COLOR1│$NC [ UDP-COSTUM : ${status_udp} ]  [ GRPC : ${status_xray} ]      [ OPEN VPN : ${status_openvpn} ] $COLOR1│$NC"
 echo -e "$COLOR1│$NC                                                          $COLOR1│$NC"
 echo -e "$COLOR1└──────────────────────────────────────────────────────────┘${NC}"
 echo -e "$COLOR1┌─────────────────────────────────────────────────────────────┐${NC}"
