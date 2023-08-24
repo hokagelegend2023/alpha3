e7ebac205d30a80558f7c68b4cf8af955ab71164
hokage <142867374+hokage2023@users.noreply.github.com>
2023-08-24 09:19:51 +0700

    Update m-system.sh

--- menu/m-system.sh
+++ menu/m-system.sh
@@ -13,11 +13,11 @@ export COLBG1="$(cat /etc/hokagevpn/theme/$colornow | grep -w "BG" | cut -d: -f2
 MYIP=$(wget -qO- ipv4.icanhazip.com);
 echo "Checking VPS"
 clear
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m          • SYSTEM MENU •          \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+echo -e "$COLOR1│${NC} ${COLBG1}             • SYSTEM MENU •                ${NC} $COLOR1│$NC"
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
 echo -e ""
-echo -e " [\e[36m•1\e[0m] Panel Domain"
+echo -e "${COLOR1}│  [01]${NC} Panel Domain $NC" 
 echo -e " [\e[36m•2\e[0m] Speedtest VPS"
 echo -e " [\e[36m•3\e[0m] Set Auto Reboot"
 echo -e " [\e[36m•4\e[0m] Restart All Service"
